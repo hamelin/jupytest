3223f007b9ff56a0217dbd999dfcf68885e28492
Benoit Hamelin <benoit@benoithamelin.com>
2020-01-06 17:31:28 -0500

    Fix long description

--- setup.py
+++ setup.py
@@ -3,8 +3,23 @@ from subprocess import run, PIPE
 import sys
 
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
+long_description = """\
+You like the super-short try-until-succeed development cycle that comes from
+implementing code in a Jupyter notebook? You would also like to perform your
+development with a bit of discipline, writing repeatable unit and integration
+tests as you go? This Python package is for you.
+
+You can start your notebook by instantiating a test suite, and as you
+test-driven-develop your code (or something), you add tests to that suite.
+These tests are run as you go. The test suite accumulates your test results
+and can produce a report at the end. The package also includes an executable
+script that will take your notebook of tests as input, run it all from the
+command line, and write a nice result report to the shell -- or some raw test
+result data structure, if that's how you fly.
+
+Find out more on `jupytest <https://github.com/hamelin/jupytest>`_'s Github
+repository.
+"""
 
 with open("requirements.txt", "r", encoding="utf-8") as f:
     requirements = [line.strip() for line in f.readlines()]
